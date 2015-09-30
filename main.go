b83548b7a241422c1726c7cf4a83908418ca2e95
Andrew Dunham <andrew@du.nham.ca>
2015-09-29 17:16:57 -0700

    Reorganize and clean up some code

--- main.go
+++ main.go
@@ -35,68 +35,9 @@ func init() {
 		"valid source IP addresses (if none given, all allowed)")
 	flag.VarP(&flagAllowedDestinationIPs, "dest-ips", "d",
 		"valid destination IP addresses (if none given, all allowed)")
+
 	flag.StringVar(&flagRemoteListener, "remote-listener", "",
 		"open the SOCKS port on the remote address (e.g. ssh://user:pass@host:port)")
-}
-
-type Rules struct{}
-
-func (r Rules) AllowConnect(dstIP net.IP, dstPort int, srcIP net.IP, srcPort int) bool {
-	log.Printf("debug: AllowConnect: %s:%d --> %s:%d", srcIP, srcPort, dstIP, dstPort)
-
-	var sourceAllowed, destAllowed bool
-
-	if len(flagAllowedSourceIPs) > 0 {
-		for _, ip := range flagAllowedSourceIPs {
-			if ip == srcIP.String() {
-				sourceAllowed = true
-			}
-		}
-	} else {
-		sourceAllowed = true
-	}
-
-	if len(flagAllowedDestinationIPs) > 0 {
-		for _, ip := range flagAllowedDestinationIPs {
-			if ip == dstIP.String() {
-				destAllowed = true
-			}
-		}
-	} else {
-		destAllowed = true
-	}
-
-	return sourceAllowed && destAllowed
-}
-
-func (r Rules) AllowBind(dstIP net.IP, dstPort int, srcIP net.IP, srcPort int) bool {
-	return false
-}
-
-func (r Rules) AllowAssociate(dstIP net.IP, dstPort int, srcIP net.IP, srcPort int) bool {
-	return false
-}
-
-func makeLogger() (*log.Logger, *colog.CoLog) {
-	// Create logger
-	logger := log.New(os.Stderr, "", 0)
-
-	// Create colog instance
-	cl := colog.NewCoLog(os.Stderr, "", 0)
-
-	// This header is from the SOCKS package, and is actually at the 'Trace'
-	// level, in that it shows all bytes copied
-	colog.AddHeader("[DEBUG] ", colog.LTrace)
-
-	// Overwrite both standard library and custom logger with this colog instance.
-	log.SetOutput(cl)
-	logger.SetOutput(cl)
-
-	// Overwrite flags on stdlib logger
-	log.SetPrefix("")
-	log.SetFlags(0)
-
-	return logger, cl
 }
 
 func main() {
@@ -139,59 +80,124 @@ func main() {
 		log.Fatalf("error: could not create SOCKS server: %s", err)
 	}
 
-	if flagRemoteListener == "" {
-		// Create SOCKS5 proxy locally
-		log.Printf("info: starting server on: %s", addr)
-		if err := server.ListenAndServe("tcp", addr); err != nil {
-			log.Fatalf("error: could not listen: %s", err)
+	// Create the listener
+	var (
+		l          net.Listener
+		listenHost string
+	)
+
+	if flagRemoteListener != "" {
+		u, err := url.Parse(flagRemoteListener)
+		if err != nil {
+			log.Fatalf("error: error parsing url: %s", err)
+		}
+		if u.Scheme != "ssh" {
+			log.Fatalf("error: url is not an SSH url: %s", flagRemoteListener)
+		}
+		if u.User == nil {
+			log.Fatalf("error: no username provided in remote listener", err)
+		}
+		if u.Path != "" || u.RawQuery != "" || u.Fragment != "" {
+			log.Printf("warning: path, query, and fragment have no meaning in remote listener URL")
 		}
 
-		return
+		listenHost = u.Host
+
+		// TODO: ssh key?
+		pass, havePass := u.User.Password()
+		if !havePass {
+			log.Fatalf("error: no password provided in remote listener", err)
+		}
+
+		config := &ssh.ClientConfig{
+			User: u.User.Username(),
+			Auth: []ssh.AuthMethod{
+				ssh.Password(pass),
+			},
+		}
+
+		sshConn, err := ssh.Dial("tcp", u.Host, config)
+		if err != nil {
+			log.Fatalf("error: error dialing remote host: %s", err)
+		}
+		defer sshConn.Close()
+
+		l, err = sshConn.Listen("tcp", addr)
+		if err != nil {
+			log.Fatalf("error: error listening on remote host: %s", err)
+		}
+	} else {
+		// Listen on a local port
+		listenHost = "localhost"
+		l, err = net.Listen("tcp", addr)
 	}
 
-	u, err := url.Parse(flagRemoteListener)
-	if err != nil {
-		log.Fatalf("error: error parsing url: %s", err)
-	}
-	if u.Scheme != "ssh" {
-		log.Fatalf("error: url is not an SSH url: %s", flagRemoteListener)
-	}
-	if u.User == nil {
-		log.Fatalf("error: no username provided in remote listener", err)
-	}
-	if u.Path != "" || u.RawQuery != "" || u.Fragment != "" {
-		log.Printf("warning: path, query, and fragment have no meaning in remote listener URL")
-	}
-
-	// TODO: ssh key?
-	pass, havePass := u.User.Password()
-	if !havePass {
-		log.Fatalf("error: no password provided in remote listener", err)
-	}
-
-	config := &ssh.ClientConfig{
-		User: u.User.Username(),
-		Auth: []ssh.AuthMethod{
-			ssh.Password(pass),
-		},
-	}
-
-	sshConn, err := ssh.Dial("tcp", u.Host, config)
-	if err != nil {
-		log.Fatalf("error: error dialing remote host: %s", err)
-	}
-	defer sshConn.Close()
-
-	l, err := sshConn.Listen("tcp", addr)
-	if err != nil {
-		log.Fatalf("error: error listening on remote host: %s", err)
-	}
 	defer l.Close()
 
-	log.Printf("info: starting socks proxy on: %s (remote addr: %s)", u.Host, addr)
+	log.Printf("info: starting socks proxy on: %s (proxy addr: %s)", listenHost, addr)
 	if err := server.Serve(l); err != nil {
 		log.Fatalf("error: could not serve socks proxy: %s", err)
 	}
 
 	log.Println("debug: done")
+}
+
+func makeLogger() (*log.Logger, *colog.CoLog) {
+	// Create logger
+	logger := log.New(os.Stderr, "", 0)
+
+	// Create colog instance
+	cl := colog.NewCoLog(os.Stderr, "", 0)
+
+	// This header is from the SOCKS package, and is actually at the 'Trace'
+	// level, in that it shows all bytes copied
+	colog.AddHeader("[DEBUG] ", colog.LTrace)
+
+	// Overwrite both standard library and custom logger with this colog instance.
+	log.SetOutput(cl)
+	logger.SetOutput(cl)
+
+	// Overwrite flags on stdlib logger
+	log.SetPrefix("")
+	log.SetFlags(0)
+
+	return logger, cl
+}
+
+type Rules struct{}
+
+func (r Rules) AllowConnect(dstIP net.IP, dstPort int, srcIP net.IP, srcPort int) bool {
+	log.Printf("debug: AllowConnect: %s:%d --> %s:%d", srcIP, srcPort, dstIP, dstPort)
+
+	var sourceAllowed, destAllowed bool
+
+	if len(flagAllowedSourceIPs) > 0 {
+		for _, ip := range flagAllowedSourceIPs {
+			if ip == srcIP.String() {
+				sourceAllowed = true
+			}
+		}
+	} else {
+		sourceAllowed = true
+	}
+
+	if len(flagAllowedDestinationIPs) > 0 {
+		for _, ip := range flagAllowedDestinationIPs {
+			if ip == dstIP.String() {
+				destAllowed = true
+			}
+		}
+	} else {
+		destAllowed = true
+	}
+
+	return sourceAllowed && destAllowed
+}
+
+func (r Rules) AllowBind(dstIP net.IP, dstPort int, srcIP net.IP, srcPort int) bool {
+	return false
+}
+
+func (r Rules) AllowAssociate(dstIP net.IP, dstPort int, srcIP net.IP, srcPort int) bool {
+	return false
 }
